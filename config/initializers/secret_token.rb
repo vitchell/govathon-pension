124df7755017e0c6fea7894f9d1ccd5444c2b483
Mitchell Lane <mitchell@fullscreen.net>
2013-11-15 21:04:40 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GovathonPension::Application.config.secret_key_base = '1323f21d3d4640d893de384af5d311bcf4374d24e124572ff3b82e5e9d21586be05515b75767782244baee92fedef59a778b10bf4764d7457de455cb22fd664b'
